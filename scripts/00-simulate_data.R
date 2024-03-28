67e9538b5c89eb638cd587d11c37936c3e84a9d5
乔魁曜 <qiao@qiaokuiyaodeMacBook-Pro.local>
2024-03-28 09:55:50 -0400

    simulate data update

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -13,7 +13,16 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+simulate_data <- 
+  tibble(
+    certainty = floor(runif(n = 1000, min = 0, max = 11)),
+    guessed_correct = floor(runif(n = 1000, min = 0, max = 2))
+  )
 
+max(simulated_data$certainty) ==10
+min(simulated_data$certainty) ==0
+unique(simulated_data$certainty) == c(0:1)
 
-
+max(simulate_data$certainty) == 10
+min(simulate_data$certainty) == 0
+setequal(unique(simulate_data$certainty), 0:10)
